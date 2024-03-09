db59c9e43ada34cd31e08aba2a64f8ff0349c505
AndrisKo <147643998+AndrisKo@users.noreply.github.com>
2024-03-09 21:06:17 +0200

    STEP 004 - DONE

--- student_Andris_Kozinda/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_Andris_Kozinda/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -9,7 +9,14 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
-        return new TravelCalculatePremiumResponse();
+
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+
+        response.setPersonFirstName(response.getPersonFirstName());
+        response.setPersonLastName(request.getPersonLastName());
+        response.setAgreementDateFrom(request.getAgreementDateFrom());
+        response.setAgreementDateTo(request.getAgreementDateTo());
+        return response;
     }
 
 }
