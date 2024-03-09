51f33e3b4c63488e18c75022acd155e18be65693
olegsradigins <oradigins@gmail.com>
2024-03-09 12:47:59 +0200

    STEP 006 - DONE

--- student_radigins_olegs/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_radigins_olegs/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -29,9 +29,46 @@ class TravelCalculatePremiumServiceImplTest {
         assertEquals(response.getPersonLastName(),lastName);
         assertEquals(response.getAgreementDateFrom(),dateFrom);
         assertEquals(response.getAgreementDateTo(),dateTo);
+    }
+    @Test
+    public void shouldTestResponseFirstNameField(){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setPersonFirstName(firstName);
+
+        TravelCalculatePremiumResponse response = travelCalculatePremiumService.calculatePremium(request);
+
+        assertEquals(response.getPersonFirstName(), firstName);
+    }
 
 
+    @Test
+    public void shouldTestResponseLastNameField(){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setPersonLastName(lastName);
 
+        TravelCalculatePremiumResponse response = travelCalculatePremiumService.calculatePremium(request);
+
+        assertEquals(response.getPersonLastName(), lastName);
+    }
+
+    @Test
+    public void shouldTestResponseDateFromField(){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setAgreementDateFrom(dateFrom);
+
+        TravelCalculatePremiumResponse response = travelCalculatePremiumService.calculatePremium(request);
+
+        assertEquals(response.getAgreementDateFrom(), dateFrom);
+    }
+
+    @Test
+    public void shouldTestResponseDateToField(){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setAgreementDateTo(dateTo);
+
+        TravelCalculatePremiumResponse response = travelCalculatePremiumService.calculatePremium(request);
+
+        assertEquals(response.getAgreementDateTo(), dateTo);
     }
 
 }
