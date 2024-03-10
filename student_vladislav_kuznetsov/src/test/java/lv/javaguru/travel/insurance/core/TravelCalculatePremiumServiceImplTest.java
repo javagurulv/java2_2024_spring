050723fc5999aeb308704a7f45ac24b7f4dfdcc7
VKuz89 <vladislav.kuznetsov89@gmail.com>
2024-03-11 00:18:20 +0200

    Step 9 - Done !

--- student_vladislav_kuznetsov/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_vladislav_kuznetsov/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -15,29 +15,23 @@ class TravelCalculatePremiumServiceImplTest {
 
     @Test
     public void checkPersonFirstName(){
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setPersonFirstName("Vladislav");
-        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
-        response = service.calculatePremium(request);
+        TravelCalculatePremiumRequest request = createRequestWithAllFields();
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getPersonFirstName(),response.getPersonFirstName());
     }
     @Test
     public void checkPersonLastName()
     {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setPersonLastName("Kuznetsov");
-        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
-        response = service.calculatePremium(request);
+        TravelCalculatePremiumRequest request = createRequestWithAllFields();
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getPersonLastName(),response.getPersonLastName());
     }
 
     @Test
     public void checkAgreementDateFrom()
     {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
-        request.setAgreementDateFrom(new Date());
-        response = service.calculatePremium(request);
+        TravelCalculatePremiumRequest request = createRequestWithAllFields();
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getAgreementDateFrom(),response.getAgreementDateFrom());
 
     }
@@ -45,12 +39,27 @@ class TravelCalculatePremiumServiceImplTest {
     @Test
     public void checkAgreementDateTo()
     {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
-        request.setAgreementDateTo(new Date());
-        response = service.calculatePremium(request);
+        TravelCalculatePremiumRequest request = createRequestWithAllFields();
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getAgreementDateTo(),response.getAgreementDateTo());
+    }
 
+    @Test
+    public void checkCalculatedAgreementPrice()
+    {
+        TravelCalculatePremiumRequest request = createRequestWithAllFields();
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+        BigDecimal agreementPrice = new BigDecimal(10);
+        assertEquals(response.getAgreementPrice(),agreementPrice);
+    }
+
+    public TravelCalculatePremiumRequest createRequestWithAllFields (){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setAgreementDateTo(new Date(2024, 3, 1));
+        request.setAgreementDateFrom(new Date(2024, 3, 11));
+        request.setPersonFirstName("Vladislav");
+        request.setPersonLastName("Kuznetsov");
+        return request;
     }
 
 
