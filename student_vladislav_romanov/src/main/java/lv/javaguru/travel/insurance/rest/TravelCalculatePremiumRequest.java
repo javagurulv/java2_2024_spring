dad4148ff5d6f3c48c99cfeb7208a9859dfd93c6
Vlad <wladislaw.romanov@gmail.com>
2024-03-12 20:41:07 +0300

    STEP 015 - DONE

--- student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -1,8 +1,14 @@
 package lv.javaguru.travel.insurance.rest;
 
+import lombok.*;
 import java.util.Date;
-import java.util.Objects;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class TravelCalculatePremiumRequest {
 
     private String personFirstName;
@@ -10,70 +16,4 @@ public class TravelCalculatePremiumRequest {
     private Date agreementDateFrom;
     private Date agreementDateTo;
 
-    public TravelCalculatePremiumRequest() { }
-
-    public TravelCalculatePremiumRequest(String personFirstName,
-                                         String personLastName,
-                                         Date agreementDateFrom,
-                                         Date agreementDateTo) {
-        this.personFirstName = personFirstName;
-        this.personLastName = personLastName;
-        this.agreementDateFrom = agreementDateFrom;
-        this.agreementDateTo = agreementDateTo;
-    }
-
-    public String getPersonFirstName() {
-        return personFirstName;
-    }
-
-    public void setPersonFirstName(String personFirstName) {
-        this.personFirstName = personFirstName;
-    }
-
-    public String getPersonLastName() {
-        return personLastName;
-    }
-
-    public void setPersonLastName(String personLastName) {
-        this.personLastName = personLastName;
-    }
-
-    public Date getAgreementDateFrom() {
-        return agreementDateFrom;
-    }
-
-    public void setAgreementDateFrom(Date agreementDateFrom) {
-        this.agreementDateFrom = agreementDateFrom;
-    }
-
-    public Date getAgreementDateTo() {
-        return agreementDateTo;
-    }
-
-    public void setAgreementDateTo(Date agreementDateTo) {
-        this.agreementDateTo = agreementDateTo;
-    }
-
-    @Override
-    public String toString() {
-        return "TravelCalculatePremiumRequest{" +
-                "personFirstName='" + personFirstName + '\'' +
-                ", personLastName='" + personLastName + '\'' +
-                ", agreementDateFrom=" + agreementDateFrom +
-                ", agreementDateTo=" + agreementDateTo +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TravelCalculatePremiumRequest that = (TravelCalculatePremiumRequest) o;
-        return Objects.equals(personFirstName, that.personFirstName) && Objects.equals(personLastName, that.personLastName) && Objects.equals(agreementDateFrom, that.agreementDateFrom) && Objects.equals(agreementDateTo, that.agreementDateTo);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(personFirstName, personLastName, agreementDateFrom, agreementDateTo);
-    }
 }
--- student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -2,8 +2,14 @@ package lv.javaguru.travel.insurance.rest;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.Objects;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class TravelCalculatePremiumResponse {
 
     private String personFirstName;
@@ -12,77 +18,4 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateTo;
     private BigDecimal agreementPrice;
 
-    public TravelCalculatePremiumResponse() {}
-
-    public TravelCalculatePremiumResponse(String personFirstName, String personLastName, Date agreementDateFrom, Date agreementDateTo, BigDecimal agreementPrice) {
-        this.personFirstName = personFirstName;
-        this.personLastName = personLastName;
-        this.agreementDateFrom = agreementDateFrom;
-        this.agreementDateTo = agreementDateTo;
-        this.agreementPrice = agreementPrice;
-    }
-
-    public String getPersonFirstName() {
-        return personFirstName;
-    }
-
-    public void setPersonFirstName(String personFirstName) {
-        this.personFirstName = personFirstName;
-    }
-
-    public String getPersonLastName() {
-        return personLastName;
-    }
-
-    public void setPersonLastName(String personLastName) {
-        this.personLastName = personLastName;
-    }
-
-    public Date getAgreementDateFrom() {
-        return agreementDateFrom;
-    }
-
-    public void setAgreementDateFrom(Date agreementDateFrom) {
-        this.agreementDateFrom = agreementDateFrom;
-    }
-
-    public Date getAgreementDateTo() {
-        return agreementDateTo;
-    }
-
-    public void setAgreementDateTo(Date agreementDateTo) {
-        this.agreementDateTo = agreementDateTo;
-    }
-
-    public BigDecimal getAgreementPrice() {
-        return agreementPrice;
-    }
-
-    public void setAgreementPrice(BigDecimal agreementPrice) {
-        this.agreementPrice = agreementPrice;
-    }
-
-    @Override
-    public String toString() {
-        return "TravelCalculatePremiumResponse{" +
-                "personFirstName='" + personFirstName + '\'' +
-                ", personLastName='" + personLastName + '\'' +
-                ", agreementDateFrom=" + agreementDateFrom +
-                ", agreementDateTo=" + agreementDateTo +
-                ", agreementPrice=" + agreementPrice +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TravelCalculatePremiumResponse that = (TravelCalculatePremiumResponse) o;
-        return Objects.equals(personFirstName, that.personFirstName) && Objects.equals(personLastName, that.personLastName) && Objects.equals(agreementDateFrom, that.agreementDateFrom) && Objects.equals(agreementDateTo, that.agreementDateTo) && Objects.equals(agreementPrice, that.agreementPrice);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(personFirstName, personLastName, agreementDateFrom, agreementDateTo, agreementPrice);
-    }
 }
