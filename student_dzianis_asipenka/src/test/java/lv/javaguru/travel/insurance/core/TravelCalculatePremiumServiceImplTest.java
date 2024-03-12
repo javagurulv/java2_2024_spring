6b314cdaa839bf79d206511545d3444d35e31311
denis-vil <denis-osipenko@mail.ru>
2024-03-12 23:01:08 +0200

    STEP 013 - DONE

--- student_dzianis_asipenka/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_dzianis_asipenka/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -3,16 +3,24 @@ package lv.javaguru.travel.insurance.core;
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class TravelCalculatePremiumServiceImplTest {
-    DateTimeService dateTimeService = new DateTimeService();
-    TravelCalculatePremiumServiceImpl calculatePremiumService = new TravelCalculatePremiumServiceImpl(dateTimeService);
+    @Mock
+    DateTimeService dateTimeService;
+    @InjectMocks
+    TravelCalculatePremiumServiceImpl calculatePremiumService;
 
     @Test
     public void expectTrueInFirstName() {
@@ -65,10 +73,10 @@ class TravelCalculatePremiumServiceImplTest {
         request.setPersonLastName("Petrov");
         request.setAgreementDateFrom(new Date());
         request.setAgreementDateTo(new Date());
+        when(dateTimeService.differenceBetweenDates(request.getAgreementDateTo(), request.getAgreementDateFrom())).thenReturn(new BigDecimal(0));
         TravelCalculatePremiumResponse actualResult = calculatePremiumService.calculatePremium(request);
         long diffInMillies = Math.abs(request.getAgreementDateTo().getTime() - request.getAgreementDateFrom().getTime());
         long daysBetween = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
-        System.out.println(daysBetween);
         assertEquals(BigDecimal.valueOf(daysBetween), actualResult.getAgreementPrice());
     }
 }
