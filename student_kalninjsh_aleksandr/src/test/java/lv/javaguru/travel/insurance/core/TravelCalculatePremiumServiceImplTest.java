cb04833f6e26092ec59aca99a746a534e55580ba
Asus <tehnokontrolj@inbox.lv>
2024-03-19 22:23:58 +0200

    Step 005

--- student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,11 +1,19 @@
 package lv.javaguru.travel.insurance.core;
 
+import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
 
 class TravelCalculatePremiumServiceImplTest {
 
+    private TravelCalculatePremiumServiceImpl premiumService = new TravelCalculatePremiumServiceImpl();
+
     @Test
-    public void deleteMe() {
+    public void personFirstName() {
+        TravelCalculatePremiumRequest premiumRequest = new TravelCalculatePremiumRequest();
+        premiumRequest.setPersonFirstName("Tom");
+        TravelCalculatePremiumResponse premiumResponse = premiumService.calculatePremium(premiumRequest);
+        assertEquals
 
     }
 
