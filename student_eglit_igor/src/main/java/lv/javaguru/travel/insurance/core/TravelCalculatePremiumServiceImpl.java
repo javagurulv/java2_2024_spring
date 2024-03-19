6160966717b9e8ca6886e9234c665452382dacdc
igor eglit <igeglits@gmail.com>
2024-03-19 12:06:22 +0200

    functional test

--- student_eglit_igor/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_eglit_igor/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -5,17 +5,18 @@ import lombok.NoArgsConstructor;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
+
+@Service
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
 
-    @Autowired
-    AgreementPriceCalculator agreementPriceCalculator;
+  @Autowired
+  AgreementPriceCalculator agreementPriceCalculator;
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
