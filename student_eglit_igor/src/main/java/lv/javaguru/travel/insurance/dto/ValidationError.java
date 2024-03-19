d332b6f932b0a7ddb765151469007c702e98abac
igor eglit <igeglits@gmail.com>
2024-03-19 21:52:54 +0200

    step 24 - done

--- student_eglit_igor/src/main/java/lv/javaguru/travel/insurance/dto/ValidationError.java
+++ student_eglit_igor/src/main/java/lv/javaguru/travel/insurance/dto/ValidationError.java
@@ -13,4 +13,11 @@ public class ValidationError {
 
     private String field;
     private String message;
+
+    @Override
+    public String toString() {
+        return "{ \n"+ "field='" + field + '\'' +
+                ", message='" + message + "\n" + '}';
+    }
 }
+
