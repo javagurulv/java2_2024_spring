a565cce0b3ba76b4826f2807179aee88a06a1871
Elīna <elina.zoldnere@gmail.com>
2024-03-20 12:04:52 +0200

    STEP 018 - DONE

--- student_elina_zoldnere/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
+++ student_elina_zoldnere/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
@@ -3,12 +3,15 @@ package lv.javaguru.travel.insurance.core;
 import lv.javaguru.travel.insurance.dto.ValidationError;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,6 +26,11 @@ public class TravelCalculatePremiumRequestValidatorTest {
 
     @InjectMocks
     private TravelCalculatePremiumRequestValidator requestValidator;
+
+    @BeforeEach
+    public void setUp() {
+        setUpRequestMockWithAllValues();
+    }
 
     @Test
     public void validate_ShouldReturnErrorWhenPersonFirstNameIsNull() {
@@ -47,12 +55,40 @@ public class TravelCalculatePremiumRequestValidatorTest {
     }
 
     @Test
-    public void validate_ShouldPassWhenPersonFirstNameIsValid() {
-        when(requestMock.getPersonFirstName()).thenReturn("Jānis");
+    public void validate_ShouldReturnErrorWhenPersonLastNameIsNull() {
+        when(requestMock.getPersonLastName()).thenReturn(null);
+
+        List<ValidationError> result = requestValidator.validate(requestMock);
+
+        assertEquals(1, result.size());
+        assertEquals("personLastName", result.get(0).getField());
+        assertEquals("Must not be empty!", result.get(0).getMessage());
+    }
+
+    @Test
+    public void validate_ShouldReturnErrorWhenPersonLastNameIsEmpty() {
+        when(requestMock.getPersonLastName()).thenReturn("");
+
+        List<ValidationError> result = requestValidator.validate(requestMock);
+
+        assertEquals(1, result.size());
+        assertEquals("personLastName", result.get(0).getField());
+        assertEquals("Must not be empty!", result.get(0).getMessage());
+    }
+
+    @Test
+    public void validate_ShouldPassWhenPersonFirstNameLastNameIsValid() {
 
         List<ValidationError> result = requestValidator.validate(requestMock);
 
         assertTrue(result.isEmpty());
+    }
+
+    private void setUpRequestMockWithAllValues() {
+        Mockito.lenient().when(requestMock.getPersonFirstName()).thenReturn("Jānis");
+        Mockito.lenient().when(requestMock.getPersonLastName()).thenReturn("Bērziņš");
+        Mockito.lenient().when(requestMock.getAgreementDateFrom()).thenReturn(new Date(124, 2, 10));
+        Mockito.lenient().when(requestMock.getAgreementDateTo()).thenReturn(new Date(124, 2, 11));
     }
 
 }
