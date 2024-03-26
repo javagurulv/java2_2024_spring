b79e2c1ff7c34665af7acdb2d509dd9553b979f6
igor eglit <igeglits@gmail.com>
2024-03-26 18:37:25 +0200

    step 37 broken

---
+++ student_eglit_igor/src/main/java/lv/javaguru/travel/insurance/core/RequestValidationPersonLastName.java
@@ -0,0 +1,16 @@
+package lv.javaguru.travel.insurance.core;
+
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class RequestValidationPersonLastName {
+    Optional<ValidationError> validatePersonLastName(TravelCalculatePremiumRequest request) {
+        return (request.getPersonLastName() == null || request.getPersonLastName().isEmpty())
+                ? Optional.of(new ValidationError("personLastName", "Must not be empty!"))
+                : Optional.empty();
+    }
+}
