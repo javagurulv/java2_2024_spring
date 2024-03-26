d6770ceb7576c8a1f72bf29da805d425ab875fbf
Elīna <elina.zoldnere@gmail.com>
2024-03-27 01:13:02 +0200

    STEP 025 - DONE

--- student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -9,6 +9,8 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,7 +19,11 @@ public class TravelCalculatePremiumResponse extends CoreResponse {
 
     private String personFirstName;
     private String personLastName;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date agreementDateFrom;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date agreementDateTo;
 
     private BigDecimal agreementPrice;
--- student_elina_zoldnere/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_elina_zoldnere/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,5 +1,7 @@
 package lv.javaguru.travel.insurance.core;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 
@@ -10,9 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.TimeZone;
 
 @ExtendWith(MockitoExtension.class)
 class TravelCalculatePremiumServiceImplTest {
@@ -62,6 +68,24 @@ class TravelCalculatePremiumServiceImplTest {
     public void calculatePremium_ShouldReturnCorrectAgreementDateTo() {
         TravelCalculatePremiumResponse response = calculatePremiumTest();
         assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
+    }
+
+    @Test
+    public void calculatePremium_ShouldReturnCorrectAgreementDateFormat() throws JsonProcessingException {
+        Date agreementDateFrom = request.getAgreementDateFrom();
+        Date agreementDateTo = request.getAgreementDateTo();
+
+        when(validateMock.validate(request)).thenReturn(emptyList());
+        when(calculateUnderwritingMock.calculateAgreementPrice(agreementDateFrom, agreementDateTo)).
+                thenReturn(BigDecimal.valueOf(1));
+        TravelCalculatePremiumResponse response = calculatePremiumTest();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setTimeZone(TimeZone.getTimeZone("EET"));
+        String responseToJson = mapper.writeValueAsString(response);
+        String expectedJson = "{\"errors\":null,\"personFirstName\":\"Jānis\",\"personLastName\":\"Bērziņš\"," +
+                "\"agreementDateFrom\":\"2024-03-10\",\"agreementDateTo\":\"2024-03-11\",\"agreementPrice\":1}";
+        assertEquals(expectedJson, responseToJson);
     }
 
     private TravelCalculatePremiumResponse calculatePremiumTest() {
