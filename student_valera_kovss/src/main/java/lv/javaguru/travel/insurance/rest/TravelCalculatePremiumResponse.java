4c8ed6581b6acb570482341c2a4a0061d25202e1
Valerijs <valerka1176@gmail.com>
2024-03-27 19:38:01 +0200

    008 DONE

--- student_valera_kovss/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_valera_kovss/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -1,5 +1,5 @@
 package lv.javaguru.travel.insurance.rest;
-
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class TravelCalculatePremiumResponse {
@@ -8,20 +8,23 @@ public class TravelCalculatePremiumResponse {
     private String personLastName;
     private Date agreementDateFrom;
     private Date agreementDateTo;
+    private BigDecimal agreementPrice;
 
-    public TravelCalculatePremiumResponse() {
-    }
+    public TravelCalculatePremiumResponse() {}
 
-    public TravelCalculatePremiumResponse(String personFirstName, String personLastName, Date agreementDateFrom, Date agreementDateTo) {
+    public TravelCalculatePremiumResponse(String personFirstName, String personLastName, Date agreementDateFrom, Date agreementDateTo, BigDecimal agreementPrice) {
+
         this.personFirstName = personFirstName;
         this.personLastName = personLastName;
         this.agreementDateFrom = agreementDateFrom;
         this.agreementDateTo = agreementDateTo;
+        this.agreementPrice = agreementPrice;
     }
 
     public String getPersonFirstName() {
         return personFirstName;
     }
+
     public void setPersonFirstName(String personFirstName) {
         this.personFirstName = personFirstName;
     }
@@ -29,6 +32,7 @@ public class TravelCalculatePremiumResponse {
     public String getPersonLastName() {
         return personLastName;
     }
+
     public void setPersonLastName(String personLastName) {
         this.personLastName = personLastName;
     }
@@ -36,6 +40,7 @@ public class TravelCalculatePremiumResponse {
     public Date getAgreementDateFrom() {
         return agreementDateFrom;
     }
+
     public void setAgreementDateFrom(Date agreementDateFrom) {
         this.agreementDateFrom = agreementDateFrom;
     }
@@ -43,8 +48,13 @@ public class TravelCalculatePremiumResponse {
     public Date getAgreementDateTo() {
         return agreementDateTo;
     }
-    public void setAgreementDateTo(Date agreementDateTo) {
-        this.agreementDateTo = agreementDateTo;
-    }
+
+    public void setAgreementDateTo(Date agreementDateTo) {this.agreementDateTo = agreementDateTo;}
+
+    public BigDecimal getAgreementPrice() {return agreementPrice;}
+
+    public void setAgreementPrice(BigDecimal agreementPrice) {this.agreementPrice = agreementPrice;}
 
 }
+
+
