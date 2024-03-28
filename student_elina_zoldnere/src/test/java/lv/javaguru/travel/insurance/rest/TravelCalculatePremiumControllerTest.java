e89595dfa0aad272b220f6eb5ea93704619f58f3
Elīna <elina.zoldnere@gmail.com>
2024-03-28 12:50:03 +0200

    STEP 027 - DONE

---
+++ student_elina_zoldnere/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -0,0 +1,44 @@
+package lv.javaguru.travel.insurance.rest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TravelCalculatePremiumControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @Test
+    public void simpleRestControllerTest() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("{" +
+                                "\"personFirstName\" : \"Jānis\",\n" +
+                                "\"personLastName\" : \"Bērziņš\",\n" +
+                                "\"agreementDateFrom\" : \"2024-03-10\",\n" +
+                                "\"agreementDateTo\" : \"2024-03-11\"\n" +
+                                "}")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is("Jānis")))
+                .andExpect(jsonPath("personLastName", is("Bērziņš")))
+                .andExpect(jsonPath("agreementDateFrom", is("2024-03-10")))
+                .andExpect(jsonPath("agreementDateTo", is("2024-03-11")))
+                .andExpect(jsonPath("agreementPrice", is(1)))
+                .andReturn();
+    }
+
+}
