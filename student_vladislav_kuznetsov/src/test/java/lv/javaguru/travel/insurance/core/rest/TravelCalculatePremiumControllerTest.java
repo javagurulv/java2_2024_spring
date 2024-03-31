6f8b0aa8f7b09f235b2fbe2f4b3863f814a04544
VKuz89 <vladislav.kuznetsov89@gmail.com>
2024-03-31 23:18:25 +0300

    STEP - 035 - Done(refactor)

--- student_vladislav_kuznetsov/src/test/java/lv/javaguru/travel/insurance/core/rest/TravelCalculatePremiumControllerTest.java
+++ student_vladislav_kuznetsov/src/test/java/lv/javaguru/travel/insurance/core/rest/TravelCalculatePremiumControllerTest.java
@@ -1,5 +1,6 @@
 package lv.javaguru.travel.insurance.core.rest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,16 +30,12 @@ public class TravelCalculatePremiumControllerTest {
     @Autowired private JsonFileReader jsonFileReader;
     @Autowired private JsonFileComparator jsonFileComparator;
 
+
     @Test
     @DisplayName("Test case 1: personFirstName is not entered")
     public void personFirstNameIsNotEntered() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_personFirstName_is_not_entered.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_personFirstName_is_not_entered.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -47,12 +44,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 2: personLastName is not entered")
     public void personLastNameIsNotEntered() throws Exception {
         String jsonRequest= "rest/TravelCalculatePremiumRequest_personLastName_is_not_entered.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_personLastName_is_not_entered.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -61,12 +53,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 3: personFirstName and personLastName is not entered")
     public void personFirstNameAndPersonLastNameIsNotEntered() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_personFirstName_and_personLastName_is_not_entered.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_personFirstName_and_personLastName_is_not_entered.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -75,12 +62,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 4: agreementDateFromIsNotEntered")
     public void agreementDateFromIsNotEntered() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_agreementDateFrom_is_not_entered.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_agreementDateFrom_is_not_entered.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -89,12 +71,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 5: agreementDateToIsNotEntered")
     public void agreementDateToIsNotEntered() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_agreementDateTo_is_not_entered.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_agreementDateTo_is_not_entered.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -103,12 +80,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 6: agreementDateTo and AgreementDateFrom IsNotEntered")
     public void agreementDateToAndAgreementDateFromIsNotEntered() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_agreementDateTo_and_agreementDateFrom_is_not_entered.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_agreementDateFrom_and_agreementDateTo_is_not_entered.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -117,12 +89,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 7: agreementDateTo is before agreementDateFrom")
     public void agreementDateToIsBeforeAgreementDateFrom() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_agreementDateTo_is_before_agreementDateFrom.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_agreementDateTo_is_before_agreementDateFrom.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -131,12 +98,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 8: agreementDateTo is equal agreementDateFrom")
     public void agreementDateToIsEqualAgreementDateFrom() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_agreementDateFrom_is_equal_agreementDateTo.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_agreementDateTo_is_equal_agreementDateFrom.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -145,12 +107,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 9: agreementDateFrom is before currentDate")
     public void agreementDateFromIsBeforeCurrentDate() throws Exception {
         String jsonRequest = "rest/TravelCalclatePremiumRequest_agreementDateFrom_is_before_currentDate.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_agreementDateFrom_is_before_currentDate.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -159,12 +116,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 10: personFirstName, personLastName, agreementDateFrom, agreementDateTo : is entered")
     public void allFieldsAreCorrect() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_all_fields_are_correct.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_all_fields_are_entered_and_are_valid.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -173,12 +125,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 11: agreementDateFrom, agreementDateTo entered in wrong Date format")
     public void agreementDateFromAndAgreementDateToIsWrongDateFormat() throws Exception {
         String jsonRequest = "rest/TravelCalclatePremiumRequest_agreementDateFrom_agreementDateTo_is_wrong_date_format.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_agreementDateFrom_and_agreementDateTo_is_wrong_format.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -187,12 +134,7 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 12: personFirstName, personLastName, agreementDateFrom, agreementDateTo : is not entered.")
     public void allFieldsAreNull() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_all_fields_are_null.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_all_fields_are_empty.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
@@ -201,13 +143,17 @@ public class TravelCalculatePremiumControllerTest {
     @DisplayName("Test case 13: PersonFirstName, personLastName, agreementDateFrom is entered. agreementDateTo is entered in wrong Date format")
     public void agreementDateToIsWrongDateFormat() throws Exception {
         String jsonRequest = "rest/TravelCalculatePremiumRequest_agreementDateTo_is_wrong_date_format.json";
-        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile(jsonRequest))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseBodyContent = result.getResponse().getContentAsString();
+        String responseBodyContent = performJsonPostRequest(jsonRequest).getResponse().getContentAsString();
         String jsonResponse = jsonFileReader.readJsonFromFile("rest/TravelCalculatePremiumResponse_when_only_agreementDateTo_is_wrong_date_format.json");
         assertTrue(jsonFileComparator.compareJsonFile(responseBodyContent,jsonResponse));
     }
+
+    public MvcResult performJsonPostRequest (String jsonRequestPath) throws Exception {
+        return mockMvc.perform(post("/insurance/travel/")
+                        .content(jsonFileReader.readJsonFromFile(jsonRequestPath))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
 }
