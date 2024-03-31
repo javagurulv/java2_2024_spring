16c79e6d651a9bda9aef2b2fc58605f06d6ad0e1
Asus <tehnokontrolj@inbox.lv>
2024-03-31 16:45:09 +0300

    Step 009 - DONE

--- student_kalninjsh_aleksandr/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_kalninjsh_aleksandr/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -4,6 +4,13 @@ import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.springframework.stereotype.Component;
 
+import javax.xml.crypto.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
@@ -14,7 +21,21 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         response.setPersonLastName(request.getPersonLastName());
         response.setAgreementDateFrom(request.getAgreementDateFrom());
         response.setAgreementDateTo(request.getAgreementDateTo());
+        response.setAgreementPrice(daysBetweenDates(request));
+
         return response;
     }
+
+    public BigDecimal daysBetweenDates(TravelCalculatePremiumRequest request) {
+        if (request.getAgreementDateFrom() != null && request.getAgreementDateTo() != null) {
+            long difference = request.getAgreementDateTo().getTime() - request.getAgreementDateFrom().getTime();
+            long daysBetween = TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
+            return BigDecimal.valueOf(daysBetween);
+        }
+        else {
+            return null;
+        }
+    }
+
 
 }
