4ce7dd74047f31a5ed31ca7c511bdb0039efe8c3
VKuz89 <vladislav.kuznetsov89@gmail.com>
2024-04-10 23:07:25 +0300

    STEP - 049, 50 DONE

--- student_vladislav_kuznetsov/src/main/java/lv/javaguru/travel/insurance/InsuranceApplication.java
+++ student_vladislav_kuznetsov/src/main/java/lv/javaguru/travel/insurance/InsuranceApplication.java
@@ -3,6 +3,7 @@ package lv.javaguru.travel.insurance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class InsuranceApplication {
 
