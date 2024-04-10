69bc3dcfd8266c94c557ce259a40caa5d3390356
Asus <tehnokontrolj@inbox.lv>
2024-04-10 23:24:07 +0300

    Step 013 - DONE

--- student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -2,61 +2,73 @@ package lv.javaguru.travel.insurance.core;
 
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class TravelCalculatePremiumServiceImplTest {
 
 
-    DateTimeService service = new DateTimeService();
-    TravelCalculatePremiumServiceImpl premiumService = new TravelCalculatePremiumServiceImpl(service);
+    private DateTimeService service;
+    private TravelCalculatePremiumServiceImpl premiumService;
+    private TravelCalculatePremiumRequest request;
+
+    @BeforeEach
+    public void setUp() {
+        request = checkingTheRequestForAllFields();
+        service = mock(DateTimeService.class);
+        when(service.calculateAgreementDaysBetweenDates(request)).thenReturn(new BigDecimal(5));
+        premiumService = new TravelCalculatePremiumServiceImpl(service);
+
+    }
 
     @Test
     public void personFirstName() {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setPersonFirstName("Tom");
         TravelCalculatePremiumResponse response = premiumService.calculatePremium(request);
         assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
     }
 
     @Test
     public void personLastName() {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setPersonLastName("Sawyer");
         TravelCalculatePremiumResponse response = premiumService.calculatePremium(request);
         assertEquals(response.getPersonLastName(), request.getPersonLastName());
     }
 
     @Test
     public void agreementDateFrom() {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setAgreementDateFrom(new Date());
         TravelCalculatePremiumResponse response = premiumService.calculatePremium(request);
         assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
     }
 
     @Test
     public void agreementDateTo() {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setAgreementDateTo(new Date());
         TravelCalculatePremiumResponse response = premiumService.calculatePremium(request);
         assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
     }
 
     @Test
     public void calculateAgreementDaysBetweenDates() {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setAgreementDateFrom(new Date(2005, 05, 15));
-        request.setAgreementDateTo(new Date(2005, 05, 20));
         TravelCalculatePremiumResponse response = premiumService.calculatePremium(request);
-        assertEquals(response.getAgreementPrice(), BigDecimal.valueOf(5));
+        assertEquals(response.getAgreementPrice(), new BigDecimal(5));
 
     }
 
+    private TravelCalculatePremiumRequest checkingTheRequestForAllFields() {
+        request = new TravelCalculatePremiumRequest();
+        request.setPersonFirstName("Tom");
+        request.setPersonLastName("Sawyer");
+        request.setAgreementDateFrom(new Date());
+        request.setAgreementDateTo(new Date());
+        return request;
+
+    }
 
 
 }
