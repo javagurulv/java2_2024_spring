362efbf46e0675569227cd5f87a62b39a165ed09
Elīna <elina.zoldnere@gmail.com>
2024-04-16 01:06:26 +0300

    STEP 069 - DONE

--- student_elina_zoldnere/src/test/java/lv/javaguru/travel/insurance/core/validations/TravelCalculatePremiumRequestValidatorImplTest.java
+++ student_elina_zoldnere/src/test/java/lv/javaguru/travel/insurance/core/validations/TravelCalculatePremiumRequestValidatorImplTest.java
@@ -8,13 +8,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TravelCalculatePremiumRequestValidatorImplTest {
@@ -22,37 +24,51 @@ public class TravelCalculatePremiumRequestValidatorImplTest {
     @Mock
     private TravelCalculatePremiumRequest requestMock;
     @Mock
-    private List<RequestFieldValidation> fieldValidationMock;
-    @Mock
     private RequestFieldValidation validation1;
     @Mock
     private RequestFieldValidation validation2;
+    @Mock
+    private List<RequestFieldValidation> fieldValidationMock;
 
     @InjectMocks
     private TravelCalculatePremiumRequestValidatorImpl requestValidator;
 
     @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
     public void validate_ShouldPassWhenAllValidationsSucceed() {
-        when(validation1.validateSingle(requestMock))
-                .thenReturn(Optional.empty());
-        when(validation2.validateSingle(requestMock))
-                .thenReturn(Optional.empty());
-        when(fieldValidationMock.stream()).thenReturn(Stream.of(validation1, validation2));
+        when(validation1.validateSingle(requestMock)).thenReturn(Optional.empty());
+        when(validation2.validateList(requestMock)).thenReturn(List.of());
 
         List<ValidationError> errors = requestValidator.validate(requestMock);
-        assertTrue(errors.isEmpty());
+
+        assertEquals(0, errors.size());
     }
 
     @Test
     public void validate_ShouldReturnErrorsWhenAllValidationsFail() {
+        AtomicInteger invocationCount = new AtomicInteger(0);
+
+        // It turns out being not so easy to call stream() on the same mock object multiple times AND with
+        // different outputs.
+        when(fieldValidationMock.stream()).thenAnswer(invocation -> {
+            if (invocationCount.getAndIncrement() == 0) {
+                return Stream.of(validation1); // first invocation
+            } else {
+                return Stream.of(validation2); // second invocation
+            }
+        });
+
         when(validation1.validateSingle(requestMock))
-                .thenReturn(Optional.of(new ValidationError()));
-        when(validation2.validateSingle(requestMock))
-                .thenReturn(Optional.of(new ValidationError()));
-        when(fieldValidationMock.stream()).thenReturn(Stream.of(validation1, validation2));
+                .thenReturn(Optional.of(new ValidationError("ERROR_CODE_1", "Description 1")));
+
+        when(validation2.validateList(requestMock))
+                .thenReturn(Arrays.asList(
+                        new ValidationError("ERROR_CODE_2", "Description 2"),
+                        new ValidationError("ERROR_CODE_3", "Description 3")));
 
         List<ValidationError> errors = requestValidator.validate(requestMock);
-        assertEquals(2, errors.size());
+
+        assertEquals(3, errors.size());
     }
 
 }
