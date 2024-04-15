cf2bddbcfdb094fe2f9ced0cbe3232a30b6ff8b2
Elīna <elina.zoldnere@gmail.com>
2024-04-16 01:06:26 +0300

    STEP 069 - DONE

--- student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelCalculatePremiumRequestValidatorImpl.java
+++ student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelCalculatePremiumRequestValidatorImpl.java
@@ -6,22 +6,39 @@ import lv.javaguru.travel.insurance.dto.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Component
 class TravelCalculatePremiumRequestValidatorImpl implements TravelCalculatePremiumRequestValidator {
 
     @Autowired
-    private List<RequestFieldValidation> fieldValidation;
+    List<RequestFieldValidation> fieldValidation;
 
-    public List<ValidationError> validate(TravelCalculatePremiumRequest request) {
+
+    @Override
+    public List <ValidationError> validate (TravelCalculatePremiumRequest request) {
+        List<ValidationError> collectedErrors = new ArrayList<>();
+        collectedErrors.addAll(collectSingleErrors(request));
+        collectedErrors.addAll(collectListErrors(request));
+        return collectedErrors;
+    }
+
+    private List<ValidationError> collectSingleErrors(TravelCalculatePremiumRequest request) {
         return fieldValidation.stream()
                 .map(validation -> validation.validateSingle(request))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
+                .collect(Collectors.toList());
+    }
+
+    private List<ValidationError> collectListErrors (TravelCalculatePremiumRequest request) {
+        return fieldValidation.stream()
+                .map(validation -> validation.validateList(request))
+                .filter((Objects::nonNull))
+                .flatMap(Collection::stream)
                 .collect(Collectors.toList());
     }
 
