df102690a855f96d9a65db208b6b119259832dd2
Valerijs <valerka1176@gmail.com>
2024-04-16 12:09:55 +0300

    015 DONE

--- student_valera_kovss/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_valera_kovss/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -5,8 +5,7 @@ import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-import java.util.concurrent.TimeUnit;
+
 
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
