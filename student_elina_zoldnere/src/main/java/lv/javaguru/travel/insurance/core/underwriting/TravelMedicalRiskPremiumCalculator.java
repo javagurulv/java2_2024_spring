c483e963577bbcc68c404c6e88e5d17793cad620
Elīna <elina.zoldnere@gmail.com>
2024-04-17 18:02:42 +0300

    STEP 073 - DONE

---
+++ student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelMedicalRiskPremiumCalculator.java
@@ -0,0 +1,30 @@
+package lv.javaguru.travel.insurance.core.underwriting;
+
+import lv.javaguru.travel.insurance.core.util.DateTimeUtil;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Component
+class TravelMedicalRiskPremiumCalculator implements TravelRiskPremiumCalculator {
+
+    @Autowired
+    private DateTimeUtil dateTimeUtil;
+
+    @Override
+    public BigDecimal calculateRiskPremium(TravelCalculatePremiumRequest request) {
+        Date agreementDateFrom = request.getAgreementDateFrom();
+        Date agreementDateTo = request.getAgreementDateTo();
+        long differenceBetweenDays = dateTimeUtil.calculateDifferenceBetweenDays(agreementDateFrom, agreementDateTo);
+        return BigDecimal.valueOf(differenceBetweenDays);
+    }
+
+    @Override
+    public String getRiskIc() {
+        return "TRAVEL_MEDICAL";
+    }
+
+}
--- student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelRiskPremiumCalculator.java
+++ student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelRiskPremiumCalculator.java
@@ -1,11 +1,13 @@
 package lv.javaguru.travel.insurance.core.underwriting;
 
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+
 import java.math.BigDecimal;
-import java.util.Date;
 
-public interface TravelCalculatePremiumUnderwriting {
+public interface TravelRiskPremiumCalculator {
 
-    BigDecimal calculateAgreementPrice(TravelCalculatePremiumRequest request);
+    public BigDecimal calculateRiskPremium(TravelCalculatePremiumRequest request);
+
+    public String getRiskIc();
 
 }
