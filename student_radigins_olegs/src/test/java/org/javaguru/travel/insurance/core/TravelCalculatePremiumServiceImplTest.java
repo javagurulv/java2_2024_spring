91899373f77fab93cf9f09673e51f118735d49d1
olegsradigins <oradigins@gmail.com>
2024-04-18 10:05:55 +0300

    STEP 06 - done

--- student_radigins_olegs/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_radigins_olegs/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -13,8 +13,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TravelCalculatePremiumServiceImplTest {
 private TravelCalculatePremiumServiceImpl travelCalculatePremiumService = new TravelCalculatePremiumServiceImpl();
+
     @Test
-    public void shouldTestResponse() {
+    public void shouldTestResponseFirstName() {
         String personFirstName = "Olegs";
         String personLastName = "Radigins";
         Date agreementDateFrom = new Date(2000, Calendar.JANUARY,1);
@@ -23,11 +24,47 @@ private TravelCalculatePremiumServiceImpl travelCalculatePremiumService = new Tr
         TravelCalculatePremiumResponse response = travelCalculatePremiumService.calculatePremium(request);
 
         assertEquals(response.getPersonFirstName(), personFirstName);
-        assertEquals(response.getPersonLastName(), personLastName);
-        assertEquals(response.getAgreementDateFrom(),agreementDateFrom);
-        assertEquals(response.getAgreementDateTo(),agreementDateTo);
-
 
     }
+
+    @Test
+    public void shouldTestResponseLastname() {
+        String personFirstName = "Olegs";
+        String personLastName = "Radigins";
+        Date agreementDateFrom = new Date(2000, Calendar.JANUARY,1);
+        Date agreementDateTo = new Date(2000, Calendar.FEBRUARY,1);
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(personFirstName,personLastName,agreementDateFrom,agreementDateTo);
+        TravelCalculatePremiumResponse response = travelCalculatePremiumService.calculatePremium(request);
+
+        assertEquals(response.getPersonLastName(), personLastName);
+
+    }
+
+    @Test
+    public void shouldTestResponseDateFrom() {
+        String personFirstName = "Olegs";
+        String personLastName = "Radigins";
+        Date agreementDateFrom = new Date(2000, Calendar.JANUARY,1);
+        Date agreementDateTo = new Date(2000, Calendar.FEBRUARY,1);
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(personFirstName,personLastName,agreementDateFrom,agreementDateTo);
+        TravelCalculatePremiumResponse response = travelCalculatePremiumService.calculatePremium(request);
+
+        assertEquals(response.getAgreementDateFrom(),agreementDateFrom);
+
+    }
+
+    @Test
+    public void shouldTestResponseDateTo() {
+        String personFirstName = "Olegs";
+        String personLastName = "Radigins";
+        Date agreementDateFrom = new Date(2000, Calendar.JANUARY,1);
+        Date agreementDateTo = new Date(2000, Calendar.FEBRUARY,1);
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(personFirstName,personLastName,agreementDateFrom,agreementDateTo);
+        TravelCalculatePremiumResponse response = travelCalculatePremiumService.calculatePremium(request);
+
+        assertEquals(response.getAgreementDateTo(),agreementDateTo);
+
+    }
+
 
 }
