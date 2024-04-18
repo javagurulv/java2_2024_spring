e61da77d56f234635f983d62be8458f3ed0f2efa
Elīna <elina.zoldnere@gmail.com>
2024-04-18 10:30:38 +0300

    STEP 076 - DONE

--- student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelCalculatePremiumUnderwritingImpl.java
+++ student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelCalculatePremiumUnderwritingImpl.java
@@ -1,5 +1,6 @@
 package lv.javaguru.travel.insurance.core.underwriting;
 
+import lv.javaguru.travel.insurance.dto.RiskPremium;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,17 +15,28 @@ class TravelCalculatePremiumUnderwritingImpl implements TravelCalculatePremiumUn
     List<TravelRiskPremiumCalculator> riskPremiumCalculators;
 
     @Override
-    public BigDecimal calculateAgreementPremium(TravelCalculatePremiumRequest request) {
+    public TravelPremiumCalculationResult calculateAgreementPremium(TravelCalculatePremiumRequest request) {
         List<String> selectedRisks = request.getSelectedRisks();
 
-        return selectedRisks.stream()
+        List<RiskPremium> riskPremiums = selectedRisks.stream()
                 .map(riskIc -> calculateRiskPremium(riskIc, request))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+                .toList();
+
+        BigDecimal agreementPremium = calculateTotalPremium(riskPremiums);
+
+        return new TravelPremiumCalculationResult(agreementPremium, riskPremiums);
     }
 
-    private BigDecimal calculateRiskPremium(String riskIc, TravelCalculatePremiumRequest request) {
+    private RiskPremium calculateRiskPremium(String riskIc, TravelCalculatePremiumRequest request) {
         var riskPremiumCalculator = findRiskPremiumCalculator(riskIc);
-        return riskPremiumCalculator.calculateRiskPremium(request);
+        BigDecimal premium = riskPremiumCalculator.calculateRiskPremium(request);
+        return new RiskPremium(riskIc, premium);
+    }
+
+    private BigDecimal calculateTotalPremium(List<RiskPremium> riskPremiums) {
+        return riskPremiums.stream()
+                .map(RiskPremium::getPremium)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     private TravelRiskPremiumCalculator findRiskPremiumCalculator(String riskIc) {
