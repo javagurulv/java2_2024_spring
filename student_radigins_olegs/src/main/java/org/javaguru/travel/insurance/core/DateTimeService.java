4ebed016c5d2b482922967bc1d4cfab1003d47a1
olegsradigins <oradigins@gmail.com>
2024-04-18 16:05:39 +0300

    STEP 12 - done

--- student_radigins_olegs/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
+++ student_radigins_olegs/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
@@ -1,10 +1,11 @@
 package org.javaguru.travel.insurance.core;
 
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-
+@Component
 class DateTimeService {
     long daysBetween(Date date1, Date date2) {
         long diff = date2.getTime() - date1.getTime();
