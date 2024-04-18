cdf6c638b833a1ebc4a33fc3119f054f9522dae5
Elīna <elina.zoldnere@gmail.com>
2024-04-18 10:35:02 +0300

    STEP 077 - DONE

--- student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/TravelCancellationRiskPremiumCalculator.java
+++ student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/TravelCancellationRiskPremiumCalculator.java
@@ -1,12 +1,13 @@
-package lv.javaguru.travel.insurance.core.underwriting;
+package lv.javaguru.travel.insurance.core.underwriting.calculators;
 
+import lv.javaguru.travel.insurance.core.underwriting.TravelRiskPremiumCalculator;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
-class TravelEvacuationRiskPremiumCalculator implements TravelRiskPremiumCalculator {
+class TravelCancellationRiskPremiumCalculator implements TravelRiskPremiumCalculator {
 
     @Override
     public BigDecimal calculateRiskPremium(TravelCalculatePremiumRequest request) {
@@ -15,7 +16,7 @@ class TravelEvacuationRiskPremiumCalculator implements TravelRiskPremiumCalculat
 
     @Override
     public String getRiskIc() {
-        return "TRAVEL_EVACUATION";
+        return "TRAVEL_CANCELLATION";
     }
 
 }
--- student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/TravelThirdPartyLiabilityRiskPremiumCalculator.java
+++ student_elina_zoldnere/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/TravelThirdPartyLiabilityRiskPremiumCalculator.java
@@ -1,12 +1,13 @@
-package lv.javaguru.travel.insurance.core.underwriting;
+package lv.javaguru.travel.insurance.core.underwriting.calculators;
 
+import lv.javaguru.travel.insurance.core.underwriting.TravelRiskPremiumCalculator;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
-class TravelLossBaggageRiskPremiumCalculator implements TravelRiskPremiumCalculator {
+class TravelThirdPartyLiabilityRiskPremiumCalculator implements TravelRiskPremiumCalculator {
 
     @Override
     public BigDecimal calculateRiskPremium(TravelCalculatePremiumRequest request) {
@@ -15,7 +16,7 @@ class TravelLossBaggageRiskPremiumCalculator implements TravelRiskPremiumCalcula
 
     @Override
     public String getRiskIc() {
-        return "TRAVEL_LOSS_BAGGAGE";
+        return "TRAVEL_THIRD_PARTY_LIABILITY";
     }
 
 }
