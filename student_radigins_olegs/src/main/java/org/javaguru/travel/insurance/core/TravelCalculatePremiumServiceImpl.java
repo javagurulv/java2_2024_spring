12af1e5a464753e19eaea209afcfc2136d097471
olegsradigins <oradigins@gmail.com>
2024-04-18 10:22:31 +0300

    STEP 09 - done

--- student_radigins_olegs/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_radigins_olegs/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -4,6 +4,9 @@ import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
+
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
@@ -14,6 +17,11 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         response.setPersonLastName(request.getPersonLastName());
         response.setAgreementDateFrom(request.getAgreementDateFrom());
         response.setAgreementDateTo(request.getAgreementDateTo());
+
+        long diff =request.getAgreementDateTo().getTime() -  request.getAgreementDateFrom().getTime();
+        long daysBetween = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        response.setAgreementPrice(new BigDecimal(daysBetween));
+
         return response;
     }
 
