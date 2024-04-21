8de0ee8fc567d9df9d4a59b80548a792e7c94db0
Vlad <wladislaw.romanov@gmail.com>
2024-04-21 19:47:35 +0300

    STEP 56 - DONE

---
+++ student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/core/validations/SelectedRisksValidator.java
@@ -0,0 +1,18 @@
+package lv.javaguru.travel.insurance.core.validations;
+
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class SelectedRisksValidator implements TravelRequestValidator {
+
+    public Optional<ValidationError> execute(TravelCalculatePremiumRequest request) {
+        return (request.getSelectedRisks() == null || request.getSelectedRisks().isEmpty())
+                ? Optional.of(new ValidationError("selectedRisks", "at least one risks must be chosen!"))
+                : Optional.empty();
+    }
+
+}
