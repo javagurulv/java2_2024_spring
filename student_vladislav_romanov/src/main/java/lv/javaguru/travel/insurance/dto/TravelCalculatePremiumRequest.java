186c18e8351101cec96e6016506564f8d42f10b7
Vlad <wladislaw.romanov@gmail.com>
2024-04-21 19:30:26 +0300

    STEP 55 - DONE

--- student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumRequest.java
+++ student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumRequest.java
@@ -1,11 +1,13 @@
 package lv.javaguru.travel.insurance.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -13,10 +15,16 @@ import java.time.LocalDate;
 public class TravelCalculatePremiumRequest {
 
     private String personFirstName;
+
     private String personLastName;
+
     @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate agreementDateFrom;
+
     @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate agreementDateTo;
+
+    @JsonAlias("selected_risks")
+    private List<String> selectedRisks;
 
 }
