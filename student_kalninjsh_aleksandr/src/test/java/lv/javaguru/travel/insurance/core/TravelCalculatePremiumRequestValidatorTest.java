79aa7360101e9cd9f0f0a539a06384b053988215
Alex-Stranger <tehnokontrolj@gmail.com>
2024-04-27 00:47:33 +0300

    Step 019; 020; 021 - Done

--- student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
+++ student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
@@ -27,6 +27,8 @@ class TravelCalculatePremiumRequestValidatorTest {
     public void returnErrorWhenPersonFirstNameIsNull() {
         when(request.getPersonFirstName()).thenReturn(null);
         when(request.getPersonLastName()).thenReturn("personLastName");
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(new Date());
         List<ValidationError> errors = requestValidator.validate(request);
         assertEquals(errors.size(), 1);
         assertEquals(errors.get(0).getField(), "personFirstName");
@@ -37,6 +39,8 @@ class TravelCalculatePremiumRequestValidatorTest {
     public void returnErrorWhenPersonFirstNameIsEmpty() {
         when(request.getPersonFirstName()).thenReturn("");
         when(request.getPersonLastName()).thenReturn("personLastName");
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(new Date());
         List<ValidationError> errors = requestValidator.validate(request);
         assertEquals(errors.size(), 1);
         assertEquals(errors.get(0).getField(), "personFirstName");
@@ -47,6 +51,8 @@ class TravelCalculatePremiumRequestValidatorTest {
     public void notReturnErrorIfThereIsPersonName() {
         when(request.getPersonFirstName()).thenReturn("Tom");
         when(request.getPersonLastName()).thenReturn("personLastName");
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(new Date());
         List<ValidationError> errors = requestValidator.validate(request);
         assertTrue(errors.isEmpty());
     }
@@ -55,6 +61,8 @@ class TravelCalculatePremiumRequestValidatorTest {
     public void returnErrorWhenPersonLastNameIsNull() {
         when(request.getPersonFirstName()).thenReturn("personFirstName");
         when(request.getPersonLastName()).thenReturn(null);
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(new Date());
         List<ValidationError> errors = requestValidator.validate(request);
         assertEquals(errors.size(), 1);
         assertEquals(errors.get(0).getField(), "personLastName");
@@ -65,6 +73,8 @@ class TravelCalculatePremiumRequestValidatorTest {
     public void returnErrorWhenPersonLastNameIsEmpty() {
         when(request.getPersonFirstName()).thenReturn("personFirstName");
         when(request.getPersonLastName()).thenReturn("");
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(new Date());
         List<ValidationError> errors = requestValidator.validate(request);
         assertEquals(errors.size(), 1);
         assertEquals(errors.get(0).getField(), "personLastName");
@@ -75,8 +85,66 @@ class TravelCalculatePremiumRequestValidatorTest {
     public void notReturnErrorIfThereIsPersonLastName() {
         when(request.getPersonFirstName()).thenReturn("personFirstName");
         when(request.getPersonLastName()).thenReturn("Sawyer");
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(new Date());
         List<ValidationError> errors = requestValidator.validate(request);
         assertTrue(errors.isEmpty());
+    }
+
+    @Test
+    public void returnErrorWhenAgreementDateFromIsNull() {
+        when(request.getPersonFirstName()).thenReturn("personFirstName");
+        when(request.getPersonLastName()).thenReturn("Sawyer");
+        when(request.getAgreementDateFrom()).thenReturn(null);
+        when(request.getAgreementDateTo()).thenReturn(new Date());
+        List<ValidationError> errors = requestValidator.validate(request);
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).getField(), "agreementDateFrom");
+        assertEquals(errors.get(0).getMessage(), "Must not be empty");
+    }
+
+    @Test
+    public void notReturnErrorIfThereIsAgreementDateFrom() {
+        when(request.getPersonFirstName()).thenReturn("personFirstName");
+        when(request.getPersonLastName()).thenReturn("personLastName");
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(new Date());
+        List<ValidationError> errors = requestValidator.validate(request);
+        assertTrue(errors.isEmpty());
+    }
+
+    @Test
+    public void returnErrorWhenAgreementDateToIsNull() {
+        when(request.getPersonFirstName()).thenReturn("personFirstName");
+        when(request.getPersonLastName()).thenReturn("personLastName");
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(null);
+        List<ValidationError> errors = requestValidator.validate(request);
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).getField(), "agreementDateTo");
+        assertEquals(errors.get(0).getMessage(), "Must not be empty");
+    }
+
+    @Test
+    public void notReturnErrorIfThereIsAgreementDateTo() {
+        when(request.getPersonFirstName()).thenReturn("personFirstName");
+        when(request.getPersonLastName()).thenReturn("personLastName");
+        when(request.getAgreementDateFrom()).thenReturn(new Date());
+        when(request.getAgreementDateTo()).thenReturn(new Date());
+        List<ValidationError> errors = requestValidator.validate(request);
+        assertTrue(errors.isEmpty());
+    }
+
+    @Test
+    public void returnErrorWhenAgreementDateFromIsAfterAgreementDateTo() {
+        when(request.getPersonFirstName()).thenReturn("personFirstName");
+        when(request.getPersonLastName()).thenReturn("personLastName");
+        when(request.getAgreementDateFrom()).thenReturn(new Date(2005, 05, 20));
+        when(request.getAgreementDateTo()).thenReturn(new Date(2005, 05, 15));
+        List<ValidationError> errors = requestValidator.validate(request);
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).getField(), "agreementDateFrom");
+        assertEquals(errors.get(0).getMessage(), "Must be less then agreementDateTo");
     }
 
 }
