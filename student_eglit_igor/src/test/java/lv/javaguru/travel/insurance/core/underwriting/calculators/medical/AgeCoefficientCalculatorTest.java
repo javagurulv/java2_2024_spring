aff012b0acacb834ae06071630c2bd4efe0dbafa
igor eglit <igeglits@gmail.com>
2024-04-27 13:57:21 +0300

    STEP 96- DONE

--- student_eglit_igor/src/test/java/lv/javaguru/travel/insurance/core/underwriting/calculators/medical/AgeCoefficientCalculatorTest.java
+++ student_eglit_igor/src/test/java/lv/javaguru/travel/insurance/core/underwriting/calculators/medical/AgeCoefficientCalculatorTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -16,7 +17,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,6 +40,7 @@ class AgeCoefficientCalculatorTest {
 
     @Test
     void shouldFindCoefficientWhenAgeCoefficientExists() {
+        ReflectionTestUtils.setField(calculator, "medicalRiskAgeCoefficientEnabled", true);
         AgeCoefficient ageCoefficient = new AgeCoefficient();
         ageCoefficient.setCoefficient(BigDecimal.valueOf(1.2));
         when(ageCoefficientRepository.findAgeCoefficientByAge(33)).thenReturn(Optional.of(ageCoefficient));
@@ -51,6 +52,7 @@ class AgeCoefficientCalculatorTest {
 
     @Test
     void shouldThrowExceptionWhenAgeCoefficientNotFound() {
+        ReflectionTestUtils.setField(calculator, "medicalRiskAgeCoefficientEnabled", true);
         when(ageCoefficientRepository.findAgeCoefficientByAge(33)).thenReturn(Optional.empty());
 
         assertThrows(RuntimeException.class, () -> calculator.findAgeCoefficient(request));
