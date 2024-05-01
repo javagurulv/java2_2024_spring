d72e0cc7199bd3577249d0db89ae1c7b8cea673f
VKuz89 <vladislav.kuznetsov89@gmail.com>
2024-05-01 10:20:46 +0300

    STEP - 072 DONE

---
+++ student_vladislav_kuznetsov/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelMedicalRiskPremiumCalculator.java
@@ -0,0 +1,27 @@
+package lv.javaguru.travel.insurance.core.underwriting;
+
+import lv.javaguru.travel.insurance.core.util.DateTimeService;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+class TravelMedicalRiskPremiumCalculator implements TravelRiskPremiumCalculator {
+
+    @Autowired
+    private DateTimeService dateTimeUtil;
+
+    @Override
+    public BigDecimal calculatePremium(TravelCalculatePremiumRequest request) {
+        var daysBetween = dateTimeUtil.calculateDaysBetweenDates(request.getAgreementDateFrom(), request.getAgreementDateTo());
+        return new BigDecimal(daysBetween);
+    }
+
+    @Override
+    public String getRiskIc() {
+        return "TRAVEL_MEDICAL";
+    }
+
+}
--- student_vladislav_kuznetsov/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwritingImpl.java
+++ student_vladislav_kuznetsov/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwritingImpl.java
@@ -6,14 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Component
-class TravelPremiumUnderwritingImpl implements TravelPremiumUnderwriting{
-    @Autowired
-    DateTimeService dateTimeService = new DateTimeService();
+class TravelPremiumUnderwritingImpl implements TravelPremiumUnderwriting {
+
+    @Autowired private List<TravelRiskPremiumCalculator> riskPremiumCalculators;
 
     @Override
     public BigDecimal calculatePremium(TravelCalculatePremiumRequest request) {
-        return BigDecimal.valueOf(dateTimeService.calculateDaysBetweenDates(request.getAgreementDateFrom(),request.getAgreementDateTo()));
+        return request.getSelectedRisks().stream()
+                .map(riskIc -> calculatePremiumForRisk(riskIc, request))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
+
+    private BigDecimal calculatePremiumForRisk(String riskIc, TravelCalculatePremiumRequest request) {
+        var riskPremiumCalculator = findRiskPremiumCalculator(riskIc);
+        return riskPremiumCalculator.calculatePremium(request);
+    }
+
+    private TravelRiskPremiumCalculator findRiskPremiumCalculator(String riskIc) {
+        return riskPremiumCalculators.stream()
+                .filter(riskCalculator -> riskCalculator.getRiskIc().equals(riskIc))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Not supported riskIc = " + riskIc));
+    }
+
 }
