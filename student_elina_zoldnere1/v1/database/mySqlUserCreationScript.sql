dc21fd8a4b276c49c7fa82a86f17223c9c8e6881
Elīna <elina.zoldnere@gmail.com>
2024-05-03 16:25:15 +0300

    STEP 114 - FIX

--- student_elina_zoldnere1/v1/database/mySqlUserCreationScript.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'InsuranceApplication'@'localhost' IDENTIFIED BY 'javaguru';
-GRANT ALL PRIVILEGES ON *.* TO 'InsuranceApplication'@'localhost';
-FLUSH PRIVILEGES;
--- Will reduce privileges later to particular database and particular operations when I will be sure,
--- that all integrations are set up correct and are working
--- student_elina_zoldnere1/v1/src/main/java/lv/javaguru/travel/insurance/config/ErrorPropertiesConfig.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.travel.insurance.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:errorCodes.properties")
-public class ErrorPropertiesConfig {
-}
