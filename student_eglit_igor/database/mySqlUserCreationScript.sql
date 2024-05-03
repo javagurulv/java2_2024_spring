5975e7fa17ea19afdb222509f28959e8365b6e44
igor eglit <igeglits@gmail.com>
2024-05-03 12:43:42 +0300

    src delete

--- student_eglit_igor/database/mySqlUserCreationScript.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'javaguru';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;
--- student_eglit_igor/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwriting.java
+++
@@ -1,7 +0,0 @@
-package lv.javaguru.travel.insurance.core.underwriting;
-
-import lv.javaguru.travel.insurance.dto.v1.TravelCalculatePremiumRequestV1;
-
-public interface TravelPremiumUnderwriting {
-    TravelPremiumCalculationResult calculateAgreementPremium(TravelCalculatePremiumRequestV1 request);
-}
