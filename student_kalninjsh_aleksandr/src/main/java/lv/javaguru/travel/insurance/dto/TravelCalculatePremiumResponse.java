80a8fee6892913b638dd3b8712c4da9bfd3a1f4d
Alex-Stranger <tehnokontrolj@gmail.com>
2024-05-04 15:48:19 +0300

    Step 025 - Done

--- student_kalninjsh_aleksandr/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ student_kalninjsh_aleksandr/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -1,5 +1,6 @@
 package lv.javaguru.travel.insurance.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +18,10 @@ public class TravelCalculatePremiumResponse extends CoreResponse {
 
     private String personFirstName;
     private String personLastName;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date agreementDateFrom;
+
     private Date agreementDateTo;
     private BigDecimal agreementPrice;
 
