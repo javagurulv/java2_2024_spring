c98c174f3fbdf322771ff06bb4816c23416ac008
Vlad <wladislaw.romanov@gmail.com>
2024-05-05 22:41:16 +0300

    STEP 72, 73 - DONE

--- student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/core/underwriting/MedicalRiskCalculator.java
+++ student_vladislav_romanov/src/main/java/lv/javaguru/travel/insurance/core/underwriting/MedicalRiskCalculator.java
@@ -1,22 +1,26 @@
 package lv.javaguru.travel.insurance.core.underwriting;
 
+import lv.javaguru.travel.insurance.core.util.DateTimeUtil;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @Component
-class TravelPremiumUnderwritingImpl implements TravelPremiumUnderwriting {
+class MedicalRiskCalculator implements TravelRiskPremiumCalculator {
 
     @Autowired
-    private List<TravelRiskPremiumCalculator> travelRiskPremiumCalculators;
+    private DateTimeUtil dateTimeService;
 
     @Override
     public BigDecimal calculatePremium(TravelCalculatePremiumRequest request) {
-        return travelRiskPremiumCalculators.stream()
-                .map(travelRiskPremiumCalculator -> travelRiskPremiumCalculator.calculatePremium(request))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return new BigDecimal(dateTimeService.calculateTravelPeriod(request.getAgreementDateFrom(), request.getAgreementDateTo()));
     }
+
+    @Override
+    public String getRiskIc() {
+        return "TRAVEL_MEDICAL";
+    }
+
 }
--- student_vladislav_romanov/src/test/java/lv/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwritingImplTest.java
+++ student_vladislav_romanov/src/test/java/lv/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwritingImplTest.java
@@ -1,15 +1,15 @@
 package lv.javaguru.travel.insurance.core.underwriting;
 
-import lv.javaguru.travel.insurance.core.util.DateTimeUtil;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -17,21 +17,22 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class TravelPremiumUnderwritingImplTest {
 
-    @Mock
-    DateTimeUtil dateTimeUtil;
+    @Mock MedicalRiskCalculator medicalRiskCalculator;
+    @Mock CancellationRiskCalculator cancellationRiskCalculator;
 
     @InjectMocks
     private TravelPremiumUnderwritingImpl travelPremiumUnderwriting;
 
     @Test
     void travelPremiumTest() {
+        BigDecimal expected = new BigDecimal(2);
+
         TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
+        var travelRiskPremiumCalculators = List.of(medicalRiskCalculator, cancellationRiskCalculator);
+        ReflectionTestUtils.setField(travelPremiumUnderwriting, "travelRiskPremiumCalculators", travelRiskPremiumCalculators);
+        when(medicalRiskCalculator.calculatePremium(any())).thenReturn(BigDecimal.ONE);
+        when(cancellationRiskCalculator.calculatePremium(any())).thenReturn(BigDecimal.ONE);
 
-        when(request.getAgreementDateFrom()).thenReturn(LocalDate.of(2030, 3, 8));
-        when(request.getAgreementDateTo()).thenReturn(LocalDate.of(2030, 3, 18));
-        when(dateTimeUtil.calculateTravelPeriod(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(10);
-
-        BigDecimal expected = new BigDecimal(10);
         BigDecimal actual = travelPremiumUnderwriting.calculatePremium(request);
 
         assertEquals(expected, actual);
