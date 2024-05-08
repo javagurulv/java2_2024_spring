44d4ccffda5807e7840b03851dfafef0dc1d0dc5
Valera <valerka1176@gmail.com>
2024-05-08 19:22:23 +0300

    STEP 26 DONE

--- student_valera_kovss/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumRequest.java
+++ student_valera_kovss/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumRequest.java
@@ -1,4 +1,5 @@
 package lv.javaguru.travel.insurance.dto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,11 @@ import java.util.Date;
 public class TravelCalculatePremiumRequest {
     private String personFirstName;
     private String personLastName;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date agreementDateFrom;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date agreementDateTo;
 
 
