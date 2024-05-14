5ce4a0b0d3b163d9a16f128ee0f1393871e78190
Alex-Stranger <tehnokontrolj@gmail.com>
2024-05-15 00:16:10 +0300

    Step 032 - Done

---
+++ student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/rest/JsonFileReader.java
@@ -0,0 +1,18 @@
+package lv.javaguru.travel.insurance.rest;
+
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class JsonFileReader {
+
+    public String readJsonFromFile(String filePath) {
+        try {
+            File file = ResourceUtils.getFile("classpath" + filePath);
+            return new String(Files.readAllBytes(file.toPath()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
