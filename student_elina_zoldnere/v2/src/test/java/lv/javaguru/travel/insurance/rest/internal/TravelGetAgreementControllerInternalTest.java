0472ccf6cb20c29d8fa6e721f5a73f54dc395ed2
Elīna <elina.zoldnere@gmail.com>
2024-05-21 12:25:31 +0300

    get agreement controller test fix

--- student_elina_zoldnere/v2/src/test/java/lv/javaguru/travel/insurance/rest/internal/TravelGetAgreementControllerInternalTest.java
+++ student_elina_zoldnere/v2/src/test/java/lv/javaguru/travel/insurance/rest/internal/TravelGetAgreementControllerInternalTest.java
@@ -2,33 +2,33 @@ package lv.javaguru.travel.insurance.rest.internal;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import lv.javaguru.travel.insurance.core.api.dto.AgreementDTO;
-import lv.javaguru.travel.insurance.core.services.EntityWriter;
+import lv.javaguru.travel.insurance.core.domain.entities.AgreementEntity;;
 import lv.javaguru.travel.insurance.rest.JsonFileReader;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.nio.charset.StandardCharsets;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.org.webcompere.modelassert.json.JsonAssertions.assertJson;
 
-@Disabled
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,14 +37,12 @@ class TravelGetAgreementControllerInternalTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @SpyBean
-    private EntityWriter entityWriter;
     @Autowired
     private JsonFileReader reader;
     @Autowired
     private Gson gson;
-
-    private String agreementUuid;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Test
     public void controller_ShouldFindExistingUuid() throws Exception {
@@ -54,13 +52,6 @@ class TravelGetAgreementControllerInternalTest {
         JsonObject setUpRequestJson = testDataJson.getAsJsonObject("set-up request");
         JsonObject expectedResponseJson = testDataJson.getAsJsonObject("expectedResponse");
 
-        doAnswer(invocation -> {
-            AgreementDTO agreement = invocation.getArgument(0);
-            this.agreementUuid = agreement.uuid();
-            System.out.println("Captured UUID: " + agreementUuid); // Test
-            return null;
-        }).when(entityWriter).writeEntities(any(AgreementDTO.class));
-
         MockHttpServletResponse setUpResponse = mockMvc.perform(post("/insurance/travel/api/v2/")
                         .content(setUpRequestJson.toString())
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
@@ -68,9 +59,12 @@ class TravelGetAgreementControllerInternalTest {
                 .andReturn()
                 .getResponse();
 
-        expectedResponseJson.addProperty("uuid", agreementUuid);
+        AgreementEntity insertedAgreement = findLastInsertedAgreement();
+        String insertedUuid = insertedAgreement.getUuid();
 
-        MockHttpServletResponse response = mockMvc.perform(get("/insurance/travel/api/internal/agreement/" + agreementUuid)
+        expectedResponseJson.addProperty("uuid", insertedUuid);
+
+        MockHttpServletResponse response = mockMvc.perform(get("/insurance/travel/api/internal/agreement/" + insertedUuid)
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andReturn()
@@ -84,6 +78,16 @@ class TravelGetAgreementControllerInternalTest {
                 .keysInAnyOrder()
                 .arrayInAnyOrder()
                 .isEqualTo(expectedResponseAsString);
+    }
+
+    private AgreementEntity findLastInsertedAgreement() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<AgreementEntity> cq = cb.createQuery(AgreementEntity.class);
+        Root<AgreementEntity> root = cq.from(AgreementEntity.class);
+        cq.orderBy(cb.desc(root.get("id")));
+        TypedQuery<AgreementEntity> query = entityManager.createQuery(cq);
+        query.setMaxResults(1);
+        return query.getSingleResult();
     }
 
 }
