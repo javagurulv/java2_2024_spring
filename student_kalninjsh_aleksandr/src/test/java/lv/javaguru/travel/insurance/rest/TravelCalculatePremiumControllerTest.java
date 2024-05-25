274d7280166218dce50c7e744a21963f1c7d9243
Alex-Stranger <tehnokontrolj@gmail.com>
2024-05-25 20:50:32 +0300

    Step 035 - Done

--- student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_kalninjsh_aleksandr/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import javax.transaction.Status;
 import java.awt.*;
 
 import static org.hamcrest.Matchers.*;
@@ -38,101 +39,67 @@ public class TravelCalculatePremiumControllerTest {
     @Test
     @DisplayName("1.personFirstName is null or empty")
     public void personFirstNameIsNullOrEmpty() throws Exception {
-        MvcResult calculatedResult = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile("rest/PremiumControllerRequest_personFirstName_is_null_or_empty.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseContent = calculatedResult.getResponse().getContentAsString();
-        String expectedJsonResponse = jsonFileReader.readJsonFromFile("rest/PremiumControllerResponse_personFirstName_is_null_or_empty.json");
-        assertEquals(mapper.readTree(responseContent), mapper.readTree(expectedJsonResponse));
-
-
+        executeAndEvaluate("rest/PremiumControllerRequest_personFirstName_is_null_or_empty.json",
+                "rest/PremiumControllerResponse_personFirstName_is_null_or_empty.json");
     }
 
     @Test
     @DisplayName("2.personLastName is null or empty")
     public void personLastNameIsNullOrEmpty() throws Exception {
-        MvcResult calculatedResult = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile("rest/PremiumControllerRequest_personLastName_is_null_or_empty.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseContent = calculatedResult.getResponse().getContentAsString();
-        String expectedJsonResponse = jsonFileReader.readJsonFromFile("rest/PremiumControllerResponse_personLastName_is_null_or_empty.json");
-        assertEquals(mapper.readTree(responseContent), mapper.readTree(expectedJsonResponse));
-
+        executeAndEvaluate
+        ("rest/PremiumControllerRequest_personLastName_is_null_or_empty.json",
+        "rest/PremiumControllerResponse_personLastName_is_null_or_empty.json");
     }
 
     @Test
     @DisplayName("3.agreementDateFrom is null or empty")
     public void agreementDateFromIsNullOrEmpty() throws Exception {
-        MvcResult calculatedResult = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile("rest/PremiumControllerRequest_agreementDateFrom_is_null_or_empty.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseContent = calculatedResult.getResponse().getContentAsString();
-        String expectedJsonResponse = jsonFileReader.readJsonFromFile("rest/PremiumControllerResponse_agreementDateFrom_is_null_or_empty.json");
-        assertEquals(mapper.readTree(responseContent), mapper.readTree(expectedJsonResponse));
+        executeAndEvaluate
+        ("rest/PremiumControllerRequest_agreementDateFrom_is_null_or_empty.json",
+        "rest/PremiumControllerResponse_agreementDateFrom_is_null_or_empty.json");
     }
 
     @Test
     @DisplayName("4.agreementDateTo is null or empty")
     public void agreementDateToIsNullOrEmpty() throws Exception {
-        MvcResult calculatedResult = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile("rest/PremiumControllerRequest_agreementDateTo_is_null_or_empty.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseContent = calculatedResult.getResponse().getContentAsString();
-        String expectedJsonResponse = jsonFileReader.readJsonFromFile("rest/PremiumControllerResponse_agreementDateTo_is_null_or_empty.json");
-        assertEquals(mapper.readTree(responseContent), mapper.readTree(expectedJsonResponse));
+        executeAndEvaluate
+        ("rest/PremiumControllerRequest_agreementDateTo_is_null_or_empty.json",
+        "rest/PremiumControllerResponse_agreementDateTo_is_null_or_empty.json");
     }
 
     @Test
     @DisplayName("5.All fields is null or empty")
     public void allFieldsIsNullOrEmpty() throws Exception {
-        MvcResult calculatedResult = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile("rest/PremiumControllerRequest_all_fields_is_null_or_empty.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseContent = calculatedResult.getResponse().getContentAsString();
-        String expectedJsonResponse = jsonFileReader.readJsonFromFile("rest/PremiumControllerResponse_all_fields_is_null_or_empty.json");
-        assertEquals(mapper.readTree(responseContent), mapper.readTree(expectedJsonResponse));
+        executeAndEvaluate
+        ("rest/PremiumControllerRequest_all_fields_is_null_or_empty.json",
+        "rest/PremiumControllerResponse_all_fields_is_null_or_empty.json");
     }
 
     @Test
     @DisplayName("6.agreementDateTo less than agreementDateFrom")
     public void agreementDateToLessThanAgreementDateFrom() throws Exception {
-        MvcResult calculatedResult = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile("rest/PremiumControllerRequest_agreementDateTo_less_than_agreementDateFrom.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseContent = calculatedResult.getResponse().getContentAsString();
-        String expectedJsonResponse = jsonFileReader.readJsonFromFile("rest/PremiumControllerResponse_agreementDateTo_less_than_agreementDateFrom.json");
-        assertEquals(mapper.readTree(responseContent), mapper.readTree(expectedJsonResponse));
+        executeAndEvaluate
+        ("rest/PremiumControllerRequest_agreementDateTo_less_than_agreementDateFrom.json",
+        "rest/PremiumControllerResponse_agreementDateTo_less_than_agreementDateFrom.json");
     }
 
     @Test
     @DisplayName("7.All fields are specified")
     public void allFieldsAreSpecified() throws Exception {
+        executeAndEvaluate
+        ("rest/PremiumControllerRequest_all_fields_are_specified.json",
+        "rest/PremiumControllerResponse_all_fields_are_specified.json");
+    }
+
+    private void executeAndEvaluate(String jsonRequestFilePath, String jsonResponseFilePath) throws Exception {
         MvcResult calculatedResult = mockMvc.perform(post("/insurance/travel/")
-                        .content(jsonFileReader.readJsonFromFile("rest/PremiumControllerRequest_all_fields_are_specified.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .content(jsonFileReader.readJsonFromFile(jsonRequestFilePath))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andReturn();
         String responseContent = calculatedResult.getResponse().getContentAsString();
-        String expectedJsonResponse = jsonFileReader.readJsonFromFile("rest/PremiumControllerResponse_all_fields_are_specified.json");
+        String expectedJsonResponse = jsonFileReader.readJsonFromFile(jsonResponseFilePath);
         assertEquals(mapper.readTree(responseContent), mapper.readTree(expectedJsonResponse));
     }
-
-
-
-
-
-
 
 }
