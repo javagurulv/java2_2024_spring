67912619dd09a1c38f9be69244ef8ef4f23e9b36
Elīna <elina.zoldnere@gmail.com>
2024-06-01 22:20:20 +0300

    STEP 174 - DONE

--- student_elina_zoldnere/v2/src/test/java/lv/javaguru/travel/insurance/loadtesting/RestCallExample.java
+++ student_elina_zoldnere/v2/src/test/java/lv/javaguru/travel/insurance/loadtesting/RestCallExample.java
@@ -6,9 +6,13 @@ import java.util.concurrent.Executors;
 class RestCallExample {
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        executorService.execute(new V1Call());
-        executorService.execute(new V2Call());
+        int numThreads = 50;
+        ExecutorService executorService = Executors.newFixedThreadPool(numThreads * 2);
+
+        for (int i = 0; i < numThreads; i++) {
+            executorService.execute(new V1Call());
+            executorService.execute(new V2Call());
+        }
 
         executorService.shutdown();
     }
