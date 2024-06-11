0f214dd0871a55bbbc64c377cc51f9c7332f2cd7
Alex-Stranger <tehnokontrolj@gmail.com>
2024-06-11 20:10:10 +0300

    Step 044-Done

--- student_kalninjsh_aleksandr/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ student_kalninjsh_aleksandr/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -1,9 +1,11 @@
 package lv.javaguru.travel.insurance.rest;
 
+import com.google.common.base.Stopwatch;
 import lv.javaguru.travel.insurance.core.TravelCalculatePremiumService;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +18,23 @@ public class TravelCalculatePremiumController {
 	@Autowired private TravelCalculatePremiumService calculatePremiumService;
 	@Autowired private TravelCalculatePremiumRequestLogger requestLogger;
 	@Autowired private TravelCalculatePremiumResponseLogger responseLogger;
+	@Autowired private TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
 
 	@PostMapping(path = "/",
 			consumes = "application/json",
 			produces = "application/json")
 	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		TravelCalculatePremiumResponse response = processRequest(request);
+		executionTimeLogger.logExecutionTime(stopwatch);
+		return response;
+	}
+
+	private TravelCalculatePremiumResponse processRequest(TravelCalculatePremiumRequest request) {
 		requestLogger.log(request);
 		TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
 		responseLogger.log(response);
 		return response;
-
 	}
 
 }
